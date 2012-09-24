7f794a4f0b09b61798aed0c4babeaa50f65ff05c
Claudio Brandolino <cbrandolino@gmail.com>
2012-09-24 07:20:56 +0200

    Implemented superneat visualization

--- js/local_audio_visualizer.js
+++ js/local_audio_visualizer.js
@@ -13,18 +13,45 @@ function init(arrayBuffer) {
     source.connect(analyser)
     analyser.connect(audioCtx.destination)
     source.noteOn(0)
-    new visualizer()
+    var viz = new simpleViz()
+    new visualizer(viz['update'])
   })
+}
+
+function simpleViz(canvas) {
+  var self = this
+  this.canvas = document.getElementById('canvas')
+  this.ctx = this.canvas.getContext("2d")
+  this.copyCtx = document.getElementById('canvas-copy').getContext("2d")
+  this.ctx.fillStyle = '#fff' 
+  this.barWidth = 10
+  this.barGap = 4
+  this.bars = Math.floor(this.canvas.width / (this.barWidth + this.barGap))
+  this.update = function(byteFreq) {
+    var step = Math.floor(byteFreq.length / self.bars)
+    self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height)
+    for (var i = 0; i < self.bars; i ++) {
+      var barHeight = byteFreq[i*step]
+      self.ctx.fillRect(
+        i * (self.barWidth + self.barGap), 
+        self.canvas.height - barHeight, 
+        self.barWidth, 
+        barHeight)
+      self.copyCtx.clearRect(0, 0, self.canvas.width, self.canvas.height)
+      self.copyCtx.drawImage(self.canvas, 0, 0)
+    }
+  }
 }
 
 function visualizer(visualization) {
   var self = this
   this.visualization = visualization  
-  var time = Date.now()
+  var last = Date.now()
   var loop = function() {
     var dt = Date.now() - last
     var byteFreq = new Uint8Array(analyser.frequencyBinCount)
     analyser.getByteFrequencyData(byteFreq)
+    last = Date.now()
     self.visualization(byteFreq, dt)
     webkitRequestAnimationFrame(loop)
   }
